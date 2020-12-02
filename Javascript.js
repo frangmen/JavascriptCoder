bed4e27f3cc547aed3871147ad2442438a5d5005
Francisco Garcia Mendez <frangm2702@gmail.com>
2020-12-01 21:38:04 -0300

    consolelogs

--- Javascript.js
+++ Javascript.js
@@ -1,19 +1,33 @@
-let nombre = prompt ("Ingrese su primer nombre");
-let apellido = prompt ("Ingrese su Apellido");
+let nombre = prompt("Ingrese su primer nombre");
+let apellido = prompt("Ingrese su Apellido");
+let edad = parseInt(prompt("Ingrese su Edad"));
+const suma = edad + 30;
+let Dia = "27";
+let Mes = "02";
+let Anio = "1989";
+const Cumpleanos = Dia + "/" + Mes + "/" + Anio;
 
-alert ("Gracias por visitarnos " + nombre);
+alert("Gracias por visitarnos " + nombre + " " + apellido);
 
-
-function saludar(nombre) {
-    console.log("Hola " + nombre + apellido + " que bueno que nos visitas");
+function saludar(nombre, apellido) {
+  console.log("Hola " + nombre + " " + apellido + " que bueno que nos visitas");
 }
+saludar(nombre, apellido);
 
-saludar(nombre + apellido);
+console.log("En el 2050 tendras " + suma + " " + "anios de vida");
 
-let result = confirm ('Estas seguro que quieres ingresar?');
+console.log(Cumpleanos);
+
+let result = confirm("Estas seguro que quieres ingresar?");
 console.log(result);
 
-var Dia = "27";
-var Mes = "02";
-var Cumpleanos = Dia + "/" + Mes;
-console.log(Cumpleanos);
+console.warn("Ya tenemos toda tu informacion");
+console.error("Deberias salir ya de esta pagina");
+console.warn("Ya tenemos toda tu informacion");
+console.error("Deberias salir ya de esta pagina");
+console.warn("Ya tenemos toda tu informacion");
+console.error("Deberias salir ya de esta pagina");
+console.warn("Ya tenemos toda tu informacion");
+console.error("Deberias salir ya de esta pagina");
+console.warn("Ya tenemos toda tu informacion");
+console.error("Deberias salir ya de esta pagina");

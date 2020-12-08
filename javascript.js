794a7bfdc3df189aab2498d79400e050f9e36a35
Francisco Garcia Mendez <frangm2702@gmail.com>
2020-12-08 20:14:57 -0300

    Clase Numero 3 , Ciclos for y condicionales

--- javascript.js
+++ javascript.js
@@ -1,11 +1,12 @@
-let nombre = prompt("Ingrese su primer nombre");
+ let nombre = prompt("Ingrese su primer nombre");
 let apellido = prompt("Ingrese su Apellido");
-let edad = parseInt(prompt("Ingrese su Edad"));
-const suma = edad + 30;
+let edad = parseInt(prompt("Ingrese su Edad")); 
+ const suma = edad + 30; 
 let Dia = "27";
 let Mes = "02";
 let Anio = "1989";
-const Cumpleanos = Dia + "/" + Mes + "/" + Anio;
+const Cumpleanos = Dia + "/" + Mes + "/" + Anio; 
+let dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
 
 alert("Gracias por visitarnos " + nombre + " " + apellido);
 
@@ -18,19 +19,17 @@ if (edad < 50)
 console.log("En el 2050 tendras " + suma + " " + "anios de vida");
 else  
 console.log("En el 2050 estaras mas cerca del arpa que de la guitarra");
-
+/* 
 console.log(Cumpleanos);
 
 let result = confirm("Estas seguro que quieres ingresar?");
 console.log(result);
+ */
 
-console.warn("Ya tenemos toda tu informacion");
-console.error("Deberias salir ya de esta pagina");
-console.warn("Ya tenemos toda tu informacion");
-console.error("Deberias salir ya de esta pagina");
-console.warn("Ya tenemos toda tu informacion");
-console.error("Deberias salir ya de esta pagina");
-console.warn("Ya tenemos toda tu informacion");
-console.error("Deberias salir ya de esta pagina");
-console.warn("Ya tenemos toda tu informacion");
-console.error("Deberias salir ya de esta pagina");
+for (let i=0; i < dias.length; i++) {
+  if (i == 6)
+  alert("Hola " + nombre + " " + apellido + " el domingo es el dia de descanso")
+
+  if (i % 2 !== 0)
+  console.log('Los dias '+dias[i]+' son pares')
+}

2999a1bbe49d7b0a7295744598e58c9a87ba994a
Francisco Garcia Mendez <frangm2702@gmail.com>
2020-12-09 20:09:01 -0300

    arreglando

--- javascript.js
+++ javascript.js
@@ -38,7 +38,7 @@ let x = Number(prompt("Cuantas horas trabajas por dia?"))
 let y = Number(prompt("Cuantos dias trabajas por semana?"))
 let multiplicar = x * y;
 let promedio = multiplicar /7 ;
-let resultado = sumar (x, y)
+
 
 function sumar (x, y) {
   console.log(`La cantidad de horas semanales que trabajas es ${x * y}`)

37fc4c62d8863ca3131d30e04abfdfe4b9c5def5
Francisco Garcia Mendez <frangm2702@gmail.com>
2020-12-17 22:34:11 -0300

    trabajo de Arrays

--- javascript.js
+++ javascript.js
@@ -61,8 +61,7 @@ else if(promedio < 10)
 alert("Estas trabajando poco, puedes trabajar un poco mas")
 
  */
-
-
+const carrito = [];
 
 function producto(nombre, tamano, precio, cantidad) {
   this.nombre = nombre;
@@ -72,31 +71,60 @@ function producto(nombre, tamano, precio, cantidad) {
   this.mostrarInfo = function () {
     console.log(`Este producto es: ${this.nombre} y cuesta: ${this.precio}`)
   }
-  
-
 }
 
 const producto1 = new producto("Pink Namaste", 2, 19.99, 1);
 const producto2 = new producto("Musculosa Blanca", 3, 22.99, 1);
-const producto3 = new producto("Remera Balanca", 2, 22.99, 1);
+const producto3 = new producto("Remera Blanca", 2, 22.99, 1);
 const producto4 = new producto("Buzo Yogi", 2, 42.99, 1);
 const producto5 = new producto("Musculosa Shakespeare", 2, 23.99, 1);
 const producto6 = new producto("Musculosa Namaste", 2, 21.99, 1);
 
-console.log(producto1);
+const Productos = new Array (producto1, producto2, producto3, producto4, producto5, producto6)
+
+carrito[0]= producto1;
+carrito[1]= producto2;
+console.log(carrito);
+
+carrito.push(producto3);
+carrito.push(producto4);
+carrito.push(producto5);
+carrito.push(producto6);
+
+console.table(carrito)
+/* console.log(producto1);
 console.log(producto2);
 console.log(producto3);
 console.log(producto4);
 console.log(producto5);
-console.log(producto6);
+console.log(producto6); */
+
+console.log(producto);
+console.log(Array.isArray(Productos));
+console.table(Productos);
+console.log(Productos.length);
+console.log(Productos[2]);
 
 
+const nombreProducto = prompt("Ingrese el nombre del producto");
+const talleArticulo = Number(prompt("Ingrese el talle eligido"));
+const precioArticulo = Number(prompt("Ingrese el precio del producto"));
+const cantidadArticulos = Number(prompt("Ingrese el numero de unidades que desearia comprar"));
 
-producto1.mostrarInfo();
+const productoUsuario = new producto(nombreProducto, talleArticulo, precioArticulo, cantidadArticulos);
+carrito.unshift(productoUsuario);
+console.table(carrito);
+console.log(Productos.length);
+console.log(Productos[0]);
+
+const resultadoFilter = carrito.filter(prop => prop.precio == 22.99);
+console.log(resultadoFilter);
+
+/* producto1.mostrarInfo();
 producto2.mostrarInfo();
 producto3.mostrarInfo();
 producto4.mostrarInfo();
 producto5.mostrarInfo();
-producto6.mostrarInfo();
+producto6.mostrarInfo(); */
 
 

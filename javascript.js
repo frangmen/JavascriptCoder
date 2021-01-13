d72695ff8d5a28d55471244278cfc7c153ae218d
Francisco Garcia Mendez <frangm2702@gmail.com>
2021-01-12 21:49:15 -0300

    Segunda entrega proyecto final Agregando Carrito

--- javascript.js
+++ javascript.js
@@ -61,7 +61,7 @@ else if(promedio < 10)
 alert("Estas trabajando poco, puedes trabajar un poco mas")
 
  */
-const carrito = [];
+/* const carrito = [];
 
 function producto(nombre, tamano, precio, cantidad) {
   this.nombre = nombre;
@@ -89,7 +89,7 @@ console.log(carrito);
 carrito.push(producto3);
 carrito.push(producto4);
 carrito.push(producto5);
-carrito.push(producto6);
+carrito.push(producto6); */
 
 /* console.table(carrito) */
 /* console.log(producto1);
@@ -99,7 +99,7 @@ console.log(producto4);
 console.log(producto5);
 console.log(producto6); */
 
-console.log(producto);
+/* console.log(producto);
 console.log(Array.isArray(Productos));
 console.table(Productos);
 console.log(Productos.length);
@@ -109,17 +109,17 @@ console.log(Productos[2]);
 const nombreProducto = prompt("Ingrese el nombre del producto");
 const talleArticulo = Number(prompt("Ingrese el talle eligido"));
 const precioArticulo = Number(prompt("Ingrese el precio del producto"));
-const cantidadArticulos = Number(prompt("Ingrese el numero de unidades que desearia comprar"));
+const cantidadArticulos = Number(prompt("Ingrese el numero de unidades que desearia comprar")); */
 
 // LOS PROMPT DE ARRIBA SON SOLO PARA EL EJERCICIO, PARA AGREGAR UN OBJETO AL ARRAY DESDE UN PROMPT
 
 // 
-
+/* 
  const productoUsuario = new producto(nombreProducto, talleArticulo, precioArticulo, cantidadArticulos);
 carrito.unshift(productoUsuario);
 console.table(carrito);
 console.log(Productos.length);
-console.log(Productos[0]);
+console.log(Productos[0]); */
 
 // FILTRANDO LOS RESULTADOS 
 /* 
@@ -132,7 +132,7 @@ producto3.mostrarInfo();
 producto4.mostrarInfo();
 producto5.mostrarInfo();
 producto6.mostrarInfo(); */
-
+/* 
 let elemento = document.createElement('h2');
 elemento.textContent = ' NEW ARRIVALS ';
 elemento.className = 'pruebas';
@@ -142,7 +142,7 @@ document.body.appendChild(elemento);
 elemento.style.color= "green";
 elemento.style.backgroundColor = "yellow";
 elemento.style.textAlign = "center";
-elemento.style.overflow = "hidden";
+elemento.style.overflow = "hidden"; */
 
 
 /* 
@@ -153,12 +153,12 @@ console.log(elemento.textContent); */
 
 // Agarrando Elementos por Clase
 
-let p = document.getElementsByTagName('p');
+/* let p = document.getElementsByTagName('p');
 console.log(p);
 
-console.log(elemento)
+console.log(elemento) */
 
-
+/* 
 function buenaBusqueda() {
   alert("Estamos trabajando para que funcione la pagina, gracias por tu paciencia");
   console.log(event);
@@ -169,7 +169,7 @@ function capturarEnter(e) {
 alert("Proximamente vas a poder buscar productos aqui por estilos");        
    }
 }
-
+*/
 function bigImg(x) {
   x.style.height = "264px";
   x.style.width = "264px";
@@ -179,4 +179,152 @@ function normalImg(x) {
   x.style.height = "132px";
   x.style.width = "132px";
 }
+ 
 
+// EN BASE A LO VISTO EN EL WORKSHOP AGREGUÉ UN CARRITO
+// QUE CONTIENE IMAGEN, NOMBRE, PRECIO Y CANTIDAD
+// A SU VEZ AGREGUE UN BOTON PARA BORRAR LA LINEA 
+// Y FUNCIONALIDAD AL BOTON DE BORRAR EL CARRITO 
+
+
+/* Selectores */
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const listaProductos = document.querySelector('#lista-productos');
+const btnVaciarCarrito = document.querySelector('#vaciar-carrito');
+
+let articulosCarrito = [];
+
+/* Listeners */
+listaProductos.addEventListener('click', agregarProducto);
+carrito.addEventListener('click', quitarProducto);
+btnVaciarCarrito.addEventListener('click', vaciarCarrito);
+
+
+document.addEventListener('DOMContentLoaded', () => {
+	articulosCarrito = JSON.parse(localStorage.getItem('carrito')) || [];
+
+	insertarCarritoHTML();
+});
+
+
+// FUNCIONES 
+function vaciarCarrito() {
+	limpiarCarrito();
+	articulosCarrito = [];
+	guardarStorage();
+}
+
+function quitarProducto(e) {
+	if (e.target.classList.contains('borrar-producto')) {
+		const productoId = e.target.getAttribute('data-id');
+
+	
+		articulosCarrito = articulosCarrito.filter(producto => producto.id != productoId);
+
+	
+		insertarCarritoHTML();
+
+
+		guardarStorage();
+	}
+}
+
+
+function agregarProducto(e) {
+
+  e.preventDefault();
+
+	if (e.target.classList.contains('btn-primary')) {
+
+    const productoSeleccionado = e.target.parentElement.parentElement;
+ 
+    obtenerDatos(productoSeleccionado);
+  }
+  }
+ 
+ 
+function obtenerDatos(producto) { 
+
+  /* Extraer informacion del producto */
+  
+ 	const productoAgregado = {
+    
+		image: producto.querySelector('img.card-img-top').src,
+	 	name: producto.querySelector('p.card-text').textContent,
+		prize: producto.querySelector('a.btn.btn-primary').textContent,
+		id: producto.querySelector('a.btn.btn-primary').getAttribute('data-id'),
+		quantity: 1 
+  }
+  const existe = articulosCarrito.some(producto => producto.id == productoAgregado.id);
+
+	if (existe) {
+		/* Producto ya existente */
+		const productos = articulosCarrito.map(producto => {
+			if (producto.id === productoAgregado.id) {
+				producto.quantity++;
+				return producto;
+			} else {
+				return producto;
+			}
+		});
+		articulosCarrito = [...productos];
+	} else {
+		/* Agrego el producto al carrito */
+		articulosCarrito.push(productoAgregado);
+	}
+
+	insertarCarritoHTML();
+	guardarStorage();
+}
+
+function guardarStorage() {
+	localStorage.setItem('carrito', JSON.stringify(articulosCarrito));
+}
+
+
+
+function insertarCarritoHTML () {
+
+  limpiarCarrito(); 
+
+  articulosCarrito.forEach( producto => {
+
+    const { image, name, prize, quantity, id } = producto;
+
+		const row = document.createElement('tr');
+		row.innerHTML = `
+			<td>
+				<img src="${image}" width=100>
+			</td>
+			<td>
+				${name}
+			</td>
+			<td>
+				${prize}
+			</td>
+			<td>
+				${quantity}
+			</td>
+			<td>
+				<a href="#" class="borrar-producto" data-id="${id}"> X </a>
+			</td>
+		`
+ 
+ 		contenedorCarrito.appendChild(row);
+	});
+} 
+
+
+function limpiarCarrito() {
+
+ while (contenedorCarrito.firstChild) {
+   contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+ }
+} 
+
+function limpiarProductos() {
+	while (listaProductos.firstChild) {
+		listaProductos.removeChild(listaProductos.firstChild);
+	}
+}
